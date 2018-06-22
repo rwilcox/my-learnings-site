f29fc1c52cb01e3cdb310a0fb2ef069043c68a60
Ryan Wilcox <rwilcox@wilcoxd.com>
2018-06-22 00:27:57 -0400

    fix JSX style issues

--- src/templates/learningTemplate.js
+++ src/templates/learningTemplate.js
@@ -9,12 +9,12 @@ export default function LearningTemplate({ data })
     const { frontmatter, html, headings } = markdownRemark
     
     return(
-        <div className="learning-container" style={{"display": "flex", "flex-direction": "row"}}>
+        <div className="learning-container" style={{"display": "flex", flexDirection: "row"}}>
 
             <div className="toc">
                 <TableOfContents headings={headings} />
             </div>
-            <div className="learning-post" style={{"padding-left": "2em"}}>
+            <div className="learning-post" style={{paddingLeft: "2em"}}>
                 <h1>Learning: { frontmatter.title }</h1>
                 <div className="learning-post-content" dangerouslySetInnerHTML={{ __html: html }} />
             </div>
